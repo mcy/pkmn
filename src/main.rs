df4328a5d024d9d1788af60288e7cc0ad413599e
Miguel Young de la Sota <mcyoung@mit.edu>
2021-03-06 14:10:20 -0500

    Add convenience aliases to pkmn::model

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use pkmn::Api;
+use pkmn::api::Cache;
+use pkmn::model;
+
 fn main() -> Result<(), pkmn::api::Error> {
-  let mut api = pkmn::Api::with_cache(pkmn::api::Cache::no_disk(256));
-  api.by_name::<pkmn::model::ability::Ability>("pressure")?;
+  let mut api = Api::with_cache(Cache::no_disk(256));
+  api.by_name::<model::Ability>("pressure")?;
 
   Ok(())
 }
--- src/model/mod.rs
+++ src/model/mod.rs
@@ -9,3 +9,13 @@ pub mod pokedex;
 pub mod region;
 pub mod species;
 pub mod version;
+
+pub use text::Language;
+pub use text::Text;
+pub use ability::Ability;
+pub use nature::Nature;
+pub use pokedex::Pokedex;
+pub use region::Region;
+pub use species::Species;
+pub use species::EggGroup;
+pub use version::Generation;
